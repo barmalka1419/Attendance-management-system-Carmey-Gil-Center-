29d662e5bf48532aee0047a299823c29852958df
barmalka1419 <116505349+barmalka1419@users.noreply.github.com>
2025-01-07 19:18:06 +0200

    Update GuideSelectionPage.js

--- AttendenceApp/fronted/src/components/GuideSelectionPage/GuideSelectionPage.js
+++ AttendenceApp/fronted/src/components/GuideSelectionPage/GuideSelectionPage.js
@@ -69,7 +69,7 @@ function GuideSelectionPage() {
                 className="guide-image"
                 onClick={() => handleGuideSelection(guide._id, guide.name)}
               />
-              <figcaption>{  translateText(guide.name)}</figcaption> 
+              <figcaption>{  guide.name}</figcaption> 
             </figure>
           ))}
       </div>
